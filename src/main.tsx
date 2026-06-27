b8c0008f287d5fe520847b9253da8e606921d005
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-06-27 14:55:36 +0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Layout from "./Layout/Layout.tsx";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import UsersPage from "./Pages/UsersPage.tsx";
-import UsersCartsPage from "./Pages/UsersCartsPage.tsx";
+import UsersPage from "./Users-Pages/UsersPage.tsx";
+import CartsPage from "./Carts-Page/CartsPage.tsx";
 import './App.css';
 
 
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
      {
          path:'/', element:<Layout/>, children:[
              {path:'users', element:<UsersPage/>},
-             {path:'/users/:id/carts', element:<UsersCartsPage/>}
+             {path:'/users/:id/carts', element:<CartsPage/>}
                  ]},
  ]);
 createRoot(document.getElementById('root')!)
