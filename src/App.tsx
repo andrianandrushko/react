2dcd070c2d93cc24e3b775994ee8288eb2f6ab59
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-06-27 14:18:37 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import './App.css'
-import {coursesArray} from "./data/coursesList.ts";
+import Component from "./components/component.tsx";
+
 
 function App() {
     return (
         <>
             {
-                coursesArray.map((course) => (
-                    <div key={course.title} className='target'>
-                        <h2> title:{course.title}</h2>
-                        <h3>monthDuration:{course.monthDuration}</h3>
-                        <h4>hourDuration:{course.hourDuration}</h4>
-                        <h5>modules:['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']</h5>
-                    </div>))
+               <Component/>
             }
         </>
     );
---
+++ src/components/component.tsx
@@ -0,0 +1,21 @@
+import {coursesArray} from "../data/coursesList.ts";
+
+const Component = () => {
+    return (
+        <>
+        <div>
+            {
+                coursesArray.map((course) => (
+                    <div key={course.title} className='target'>
+                        <h2> title:{course.title}</h2>
+                        <h3>monthDuration:{course.monthDuration}</h3>
+                        <h4>hourDuration:{course.hourDuration}</h4>
+                        <h5>modules:['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']</h5>
+                    </div>))
+            }
+        </div>
+        </>
+    );
+};
+
+export default Component;
