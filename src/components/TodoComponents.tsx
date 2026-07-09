7d521833782379486d6e663efa9d099a1cf7b12e
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-07-09 15:31:52 +0300

    commit

--- src/components/TodoComponents.tsx
+++ src/components/TodoComponents.tsx
@@ -14,8 +14,8 @@ const TodoComponents = () => {
 
     return (
         <div>
-            {todos.map((todo) => (
-                <div key={todo.id} className="cool">
+            {todos.map((todo, index ) => (
+                <div key={index} className="cool">
                     <h2>userId: {todo.userId}</h2>
                     <h3>Id: {todo.id}</h3>
                     <h4>Title: {todo.title}</h4>
