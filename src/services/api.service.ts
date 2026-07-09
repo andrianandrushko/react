abe8d476d321837d123b17994fd87800231913c3
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-07-09 20:10:26 +0300

    commit

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -3,10 +3,8 @@ import type {IProducts} from "../models/IProducts.ts";
 const baseUrl = 'https://dummyjson.com/products';
 
 export const getProducts = async (): Promise<IProducts[]> => {
-
     const response = await fetch(baseUrl);
-
     const data = await response.json();
 
-    return data.comments;
+    return data.products;
 };
