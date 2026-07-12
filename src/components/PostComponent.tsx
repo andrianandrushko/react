9d3156303dec111867682cbe2f74093443305e55
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-07-12 13:03:20 +0300

    commit

--- src/components/PostComponent.tsx
+++ src/components/PostComponent.tsx
@@ -1,23 +1,21 @@
-import type {IPosts} from "../models/IPosts.ts";
+import type { IPosts } from "../models/IPosts.ts";
 
 interface Props {
     post: IPosts;
 }
 
-const ProductComponent = ({ post }: Props) => {
+const PostComponent = ({ post }: Props) => {
     return (
-        <div>
-            <p>id:{post.id}</p>
-            <p>title:{post.title}</p>
-            <p>body:{post.body}</p>
-            <p>tags:{post.tags}</p>
-            <p>likes:{post.reactions.likes}</p>
-            <p>dislikes:{post.reactions.dislikes}</p>
-            <p>views:{post.views}</p>
+        <div className="cool">
+            <p>id: {post.id}</p>
+            <p>title: {post.title}</p>
+            <p>body: {post.body}</p>
+            <p>tags: {post.tags}</p>
+            <p>likes: {post.reactions.likes}</p>
+            <p>dislikes: {post.reactions.dislikes}</p>
+            <p>views: {post.views}</p>
         </div>
     );
 };
 
-
-
-export default ProductComponent;
+export default PostComponent;
--- src/components/PostComponents.tsx
+++ src/components/PostComponents.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react";
 import type {IPosts} from "../models/IPosts.ts";
 import {getPosts} from "../services/api.service.ts";
+import PostComponent from "./PostComponent.tsx";
 
 const PostComponents = () => {
     const [posts, setPosts] = useState<IPosts[]>([])
@@ -14,16 +15,9 @@ const PostComponents = () => {
     return (
         <>
             {
-                posts.map((post, index) => (
-                    <div key={index}  className={'cool'}>
-                        <p>id:{post.id}</p>
-                        <p>title:{post.title}</p>
-                        <p>body:{post.body}</p>
-                        <p>tags:{post.tags}</p>
-                        <p>likes:{post.reactions.likes}</p>
-                        <p>dislikes:{post.reactions.dislikes}</p>
-                        <p>views:{post.views}</p>
-                    </div>)
+                posts.map((post) => (
+                    <PostComponent key={post.id} post={post} />
+                   )
                 )
             }
         </>
