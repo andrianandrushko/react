4c2f793b68713aff1cefd1a6e1c9f1250aebcde4
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-07-12 22:50:32 +0300

    commit

--- src/Pages/PostsPage.tsx
+++ src/Pages/PostsPage.tsx
@@ -1,11 +1,26 @@
-import {Outlet} from "react-router-dom";
-import PostsComponents from "../components/PostsComponents.tsx";
+import { useEffect, useState } from "react";
+import type { IPosts } from "../models/IPosts.ts";
+import { getPosts } from "../services/api.service.ts";
 
 const PostsPage = () => {
+    const [posts, setPosts] = useState<IPosts[]>([]);
+
+    useEffect(() => {
+        getPosts().then(data => {
+            setPosts(data);
+        });
+    }, []);
+
     return (
         <div>
-            <PostsComponents/>
-                <Outlet/>
+            <h1>Posts</h1>
+
+            {posts.map(post => (
+                <div key={post.id}>
+                    <h3>{post.title}</h3>
+                    <p>{post.body}</p>
+                </div>
+            ))}
         </div>
     );
 };
--- src/Pages/UsersPage.tsx
+++ src/Pages/UsersPage.tsx
@@ -1,11 +1,26 @@
-import { Outlet } from "react-router-dom";
-import UsersComponents from "../components/UsersComponents.tsx";
+import { useEffect, useState } from "react";
+import type { IUsers } from "../models/IUsers.ts";
+import { getUsers } from "../services/api.service.ts";
 
 const UsersPage = () => {
+    const [users, setUsers] = useState<IUsers[]>([]);
+
+    useEffect(() => {
+        getUsers().then(data => {
+            setUsers(data);
+        });
+    }, []);
+
     return (
         <div>
-            <UsersComponents />
-            <Outlet />
+            <h1>Users</h1>
+
+            {users.map(user => (
+                <div key={user.id}>
+                    <h3>{user.firstName} {user.lastName}</h3>
+                    <p>{user.email}</p>
+                </div>
+            ))}
         </div>
     );
 };
