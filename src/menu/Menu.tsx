59eacdc9d6261e1709ba58bbcf60fe9ff225b89f
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-07-15 19:57:00 +0300

    commit

--- src/menu/Menu.tsx
+++ src/menu/Menu.tsx
@@ -6,25 +6,18 @@ const Menu = () => {
         <div className="menu">
             // список посилань на сторінки
             <ul>
-                // посилання на головну сторінку
                 <li><Link to="/">Home</Link></li>
 
-                // посилання на сторінку логіну
                 <li><Link to="/login">Login</Link></li>
 
-                // посилання на сторінку авторизаційних ресурсів
                 <li><Link to="/auth/resources">Auth Resources</Link></li>
 
-                // посилання на сторінку постів
                 <li><Link to="/posts">Posts</Link></li>
 
-                // посилання на сторінку коментарів
                 <li><Link to="/comments">Comments</Link></li>
 
-                // посилання на сторінку задач
                 <li><Link to="/todos">Todos</Link></li>
 
-                // посилання на сторінку цитат
                 <li><Link to="/quotes">Quotes</Link></li>
             </ul>
         </div>
