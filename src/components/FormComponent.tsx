098f385ec18a000b986197a93b69cceb7f92246e
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-07-17 14:41:30 +0300

    commit

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -2,7 +2,7 @@ import {useForm} from "react-hook-form";
 import {joiResolver} from "@hookform/resolvers/joi";
 import FormValidators from "../validators/Form.Validators.tsx";
 import {CreateCar} from "../services/api.service.ts";
-import {data} from "react-router";
+
 
 
 
